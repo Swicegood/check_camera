470cff0d1a37079f51e4c6056f0a5a56ba96d02b
Jagadguru <jguru108@gmail.com>
2017-06-08 20:50:02 -0400

    Working  so far but needs case statement and pramaterization

--- check_camera.py
+++ check_camera.py
@@ -7,7 +7,12 @@
 #by Jeff Swicegood (Jaga) and Bob Gailer May 1,2017
 
 import urllib.request
+
+PREFIX = b"<td class=\"colEvents\"><a href=\"?view=events&amp;page=1&amp;filter[terms][0][attr]=DateTime&amp;filter[terms][0][op]=%3E%3D&amp;filter[terms][0][val]=-1+day&amp;filter[terms][1][cnj]=and&amp;filter[terms][1][attr]=MonitorId&amp;filter[terms][1][op]=%3D&amp;filter[terms][1][val]=3\" onclick=\"createPopup( '?view=events&amp;page=1&amp;filter[terms][0][attr]=DateTime&amp;filter[terms][0][op]=%3E%3D&amp;filter[terms][0][val]=-1+day&amp;filter[terms][1][cnj]=and&amp;filter[terms][1][attr]=MonitorId&amp;filter[terms][1][op]=%3D&amp;filter[terms][1][val]=3\', 'zmEvents\', 'events\' ); return( false );\">"
+OFFSET = len(PREFIX)
+
 page = urllib.request.urlopen('http://192.168.0.70/zm/index.php',
                               b'action=login&view=console&username=admin&password=xxxxxx')
 page2 = page.read()
-page2.find(b'<td class="colEvents"><a href="?view=events&amp;page=1&amp;filter[terms][0][attr]=DateTime&amp;filter[terms][0][op]=%3E%3D&amp;filter[terms][0][val]=-1+day&amp;filter[terms][1][cnj]=and&amp;filter[terms][1][attr]=MonitorId&amp;filter[terms][1][op]=%3D&amp;filter[terms][1][val]=3" onclick="createPopup( '?view=events&amp;page=1&amp;filter[terms][0][attr]=DateTime&amp;filter[terms][0][op]=%3E%3D&amp;filter[terms][0][val]=-1+day&amp;filter[terms][1][cnj]=and&amp;filter[terms][1][attr]=MonitorId&amp;filter[terms][1][op]=%3D&amp;filter[terms][1][val]=3', 'zmEvents', 'events' ); return( false );">')
+i = page2.find(PREFIX) + OFFSET 
+numberofevents = page2[i]
