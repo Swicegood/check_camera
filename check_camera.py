dffa0e7e2fcfd585fb8dc50a4279157fb921a5c6
Jagadguru <jguru108@gmail.com>
2017-06-08 21:23:44 -0400

    Changed shebang so urllib.request would work on linux. Requires python 3.

--- check_camera.py
+++ check_camera.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 #This is a Nagios plugin that checks a camera specified by the command line augument
 #it works by parsing the Zoneminder server homepage to find the number of camera events
@@ -6,15 +6,17 @@
 
 #by Jeff Swicegood (Jaga) and Bob Gailer May 1,2017
 
+import sys
 import urllib.request
 
-camera_number = "3".encode('ascii')
+camera_number = sys.argv[1].encode('ascii')
 
-PREFIX = b"<td class=\"colEvents\"><a href=\"?view=events&amp;page=1&amp;filter[terms][0][attr]=DateTime&amp;filter[terms][0][op]=%3E%3D&amp;filter[terms][0][val]=-1+day&amp;filter[terms][1][cnj]=and&amp;filter[terms][1][attr]=MonitorId&amp;filter[terms][1][op]=%3D&amp;filter[terms][1][val]="+ camera_number + b"\" onclick=\"createPopup( '?view=events&amp;page=1&amp;filter[terms][0][attr]=DateTime&amp;filter[terms][0][op]=%3E%3D&amp;filter[terms][0][val]=-1+day&amp;filter[terms][1][cnj]=and&amp;filter[terms][1][attr]=MonitorId&amp;filter[terms][1][op]=%3D&amp;filter[terms][1][val]="+ camera_number + b"\', 'zmEvents\', 'events\' ); return( false );\">"
-OFFSET = len(PREFIX)
+prefix = b"<td class=\"colEvents\"><a href=\"?view=events&amp;page=1&amp;filter[terms][0][attr]=DateTime&amp;filter[terms][0][op]=%3E%3D&amp;filter[terms][0][val]=-1+day&amp;filter[terms][1][cnj]=and&amp;filter[terms][1][attr]=MonitorId&amp;filter[terms][1][op]=%3D&amp;filter[terms][1][val]="+ camera_number + b"\" onclick=\"createPopup( '?view=events&amp;page=1&amp;filter[terms][0][attr]=DateTime&amp;filter[terms][0][op]=%3E%3D&amp;filter[terms][0][val]=-1+day&amp;filter[terms][1][cnj]=and&amp;filter[terms][1][attr]=MonitorId&amp;filter[terms][1][op]=%3D&amp;filter[terms][1][val]="+ camera_number + b"\', 'zmEvents\', 'events\' ); return( false );\">"
+offset = len(prefix)
 
 page = urllib.request.urlopen('http://192.168.0.70/zm/index.php',
                               b'action=login&view=console&username=admin&password=xxxxxx')
 page2 = page.read()
-i = page2.find(PREFIX) + OFFSET 
+i = page2.find(prefix) + offset 
 numberofevents = page2[i]
+print(page2[i:i+2])
