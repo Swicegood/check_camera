d53805d00769907c35ba5a55011d9c8b5963185a
Swicegood <jguru108@gmail.com>
2019-06-02 04:08:37 -0400

    added ZM API calls

--- check_camera.py
+++ check_camera.py
@@ -1,28 +1,67 @@
-#!/usr/bin/python3
+#!/usr/bin/python2.6
 
-#This is a Nagios plugin that checks a camera specified by the command line augument
-#it works by parsing the Zoneminder server homepage to find the number of camera events
-#today and reterning "CRITICAL" if there are none.
+# This is a Nagios plugin that checks a camera specified by the command line argument
+# it works by parsing the Zoneminder server homepage to find the number of camera events
+# today and returning "CRITICAL" if there are none.
 
-#by Jeff Swicegood (Jaga) and Bob Gailer May 1,2017
+# by Jeff Swicegood (Jaga) and Bob Gailer May 1,2017
 
-import sys
-import urllib.request
 
-camera_number = sys.argv[1].encode('ascii')
+import urllib2
+import cookielib
+import json
 
-prefix = b"<td class=\"colEvents\"><a href=\"?view=events&amp;page=1&amp;filter[terms][0][attr]=DateTime&amp;filter[terms][0][op]=%3E%3D&amp;filter[terms][0][val]=-1+day&amp;filter[terms][1][cnj]=and&amp;filter[terms][1][attr]=MonitorId&amp;filter[terms][1][op]=%3D&amp;filter[terms][1][val]="+ camera_number + b"\" onclick=\"createPopup( '?view=events&amp;page=1&amp;filter[terms][0][attr]=DateTime&amp;filter[terms][0][op]=%3E%3D&amp;filter[terms][0][val]=-1+day&amp;filter[terms][1][cnj]=and&amp;filter[terms][1][attr]=MonitorId&amp;filter[terms][1][op]=%3D&amp;filter[terms][1][val]="+ camera_number + b"\', 'zmEvents\', 'events\' ); return( false );\">"
-offset = len(prefix)
 
-page = urllib.request.urlopen('http://192.168.0.70/zm/index.php',
-                              b'action=login&view=console&username=admin&password=xxxxxxx')
-page2 = page.read()
-i = page2.find(prefix) + offset
-numberofevents = page2[i] - 48   #get ASCII and convert to actual number (only 1 signicant digit)
+number_of_monitors_defined = 10
 
-if numberofevents:
-    print("OK - There are camera events for today.")
-    exit(0)
-else:
-    print("CRITICAL - No camera events today.")
-    exit(2)
+
+req = urllib2.Request('http://192.168.0.70/zm/index.php',
+                      b'action=login&view=console&username=admin&password=xxxxx')
+
+page = urllib2.urlopen(req)
+
+c = cookielib.CookieJar()
+
+cookielib.CookieJar.extract_cookies(c, page, req)
+
+req2 = urllib2.Request('http://192.168.0.70/zm/api/events/consoleEvents/24%20hour.json')
+
+cookielib.CookieJar.add_cookie_header(c, req2)
+
+page2 = urllib2.urlopen(req2)
+
+page2 = page2.read()
+
+oneday_data = json.loads(page2)
+
+req3 = urllib2.Request('http://192.168.0.70/zm/api/monitors.json')
+
+page3 = urllib2.urlopen(req3)
+
+monitors_data = json.loads(page3.read())
+
+def get_monitor_name(mon_number):
+    for monitor in monitors_data['monitors']:
+        if (monitor['Monitor_Status']['Status'] == 'Connected') and (monitor['Monitor_Status']['MonitorId'] == mon_number):
+            return monitor['Monitor']['Name']
+    return "Not Found"
+
+count = 0
+
+message=''
+
+for results, monitor in oneday_data.iteritems():
+    for i in monitor.iteritems():
+        if not int(i[1]):
+            monitor_name = get_monitor_name(i[0])
+            message = (message + 'CRITICAL - CAMERA ' + monitor_name.encode('UTF8') + ' has 0 events today!')
+    if message != '':
+        print (message)
+        exit(2)
+    else:
+        for results, monitor in oneday_data.iteritems():
+            for i in monitor.iteritems():
+                message = (message + ' ' + (str(i[0].encode('UTF8')) + ':' +str(i[1].encode('UTF8'))))
+        message = "OK - " + message
+        print (message)
+        exit(0)
