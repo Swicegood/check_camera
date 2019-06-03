2b5da368a119b7e66ec7679578f41dd1cb23e203
Swicegood <jguru108@gmail.com>
2019-06-03 01:40:44 -0400

    fix bad reporting

--- check_camera.py
+++ check_camera.py
@@ -1,18 +1,15 @@
-#!/usr/bin/python2.6
+#!/usr/bin/python2
 
 # This is a Nagios plugin that checks a camera specified by the command line argument
 # it works by parsing the Zoneminder server homepage to find the number of camera events
 # today and returning "CRITICAL" if there are none.
 
-# by Jeff Swicegood (Jaga) and Bob Gailer May 1,2017
+# by Jeff Swicegood (Jaga) and Bob Gailer May 1,2017,2019
 
 
 import urllib2
 import cookielib
 import json
-
-
-number_of_monitors_defined = 10
 
 
 req = urllib2.Request('http://192.168.0.70/zm/index.php',
@@ -42,26 +39,35 @@ monitors_data = json.loads(page3.read())
 
 def get_monitor_name(mon_number):
     for monitor in monitors_data['monitors']:
-        if (monitor['Monitor_Status']['Status'] == 'Connected') and (monitor['Monitor_Status']['MonitorId'] == mon_number):
+        if (monitor['Monitor_Status']['Status'] == 'Connected') and (monitor['Monitor_Status']['MonitorId']  == mon_number):
             return monitor['Monitor']['Name']
     return "Not Found"
 
-count = 0
+def get_connected_monitors():
+    connected = []
+    for monitor in monitors_data['monitors']:
+        if (monitor['Monitor_Status']['Status'] == 'Connected'):
+            connected.append(monitor['Monitor_Status']['MonitorId'])
+    return connected
+
+
+bad_monitors = {k for k in get_connected_monitors() if str(k) not in oneday_data['results']}  # type: set
 
 message=''
 
-for results, monitor in oneday_data.iteritems():
-    for i in monitor.iteritems():
-        if not int(i[1]):
-            monitor_name = get_monitor_name(i[0])
-            message = (message + 'CRITICAL - CAMERA ' + monitor_name.encode('UTF8') + ' has 0 events today!')
-    if message != '':
-        print (message)
-        exit(2)
-    else:
-        for results, monitor in oneday_data.iteritems():
-            for i in monitor.iteritems():
-                message = (message + ' ' + (str(i[0].encode('UTF8')) + ':' +str(i[1].encode('UTF8'))))
-        message = "OK - " + message
-        print (message)
-        exit(0)
+for monitor in bad_monitors:
+    monitor_name = get_monitor_name(monitor)
+    message = (message + 'CRITICAL - CAMERA ' + monitor_name.encode('UTF8') + ' has 0 events today!')
+if message != '':
+    print (message)
+    exit(2)
+else:
+    for results, monitor in oneday_data.iteritems():
+        for i in monitor.iteritems():
+            message = (message + ' ' + (str(i[0].encode('UTF8')) + ':' + str(i[1].encode('UTF8'))))
+    message = "OK - " + message
+    print (message)
+    exit(0)
+
+
+
